65fcfa796fce8de7b48709da08d0578944da7f48
grassshrimp <grassshrimp761017@gmail.com>
2016-03-25 22:05:16 +0800

    use Twig as default template engine

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -3,6 +3,19 @@
 
 /* load the HMVC_Loader class */
 require APPPATH . 'third_party/HMVC/Loader.php';
+require APPPATH . 'libraries/Twig.php';
 
 class MY_Loader extends HMVC_Loader {
+  public function __construct() {
+    parent::__construct();
+    $this->twig = new Twig;
+  }
+
+  public function get_vars(){
+    return $this->_ci_cached_vars;
+  }
+
+  public function view($template, $data = array(), $return = FALSE){
+    return $this->twig->parse($template, $data, $return);
+  }
 }
